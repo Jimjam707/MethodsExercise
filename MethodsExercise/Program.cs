f31881cb9d8b05cee5a4bffc14d942a2f28fb0f8
Jimi Adams <jimi-8@live.com>
2025-05-02 17:32:25 -0400

    Excercises and challenge completed

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,60 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        
+        public static void MadLib()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}! What is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favoriteAnimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string favoriteBand = Console.ReadLine();
+            
+            Console.WriteLine($"One day, {userName}, was walking through the woods while wearing a {favoriteColor} t-shirt. {userName} saw a {favoriteAnimal} listening to {favoriteBand}. ");
+            
         }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
+        }
+
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Add(2, 2));
+            Console.WriteLine(Subtract(2, 2));
+            Console.WriteLine(Multiply(3, 4));
+            Console.WriteLine(Divide(2,2));
+            Console.WriteLine(Sum(2,4,2,4,6,1,1,1,1,1));
+        }
+        
+        
     }
 }
